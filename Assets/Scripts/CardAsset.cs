df98cb0cd97805673786f0342615c66c3e81de46
zsezs <alireza1376@gmail.com>
2018-12-12 20:34:45 +0330

    init

---
+++ Assets/Scripts/CardAsset.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAsset : ScriptableObject {
+
+    public int power = 0;
+    public string name;
+	
+}
